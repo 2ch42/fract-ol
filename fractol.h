04d0ffdb9c723632e9953a06d0ed192705d9b115
Changhyeon Lee <changhyl@student.42seoul.kr>
2023-03-23 21:17:37 +0900

    1

--- fractol.h
+++ fractol.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: changhyl <changhyl@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 21:14:00 by changhyl          #+#    #+#             */
+/*   Updated: 2023/03/23 21:15:26 by changhyl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
@@ -10,11 +22,11 @@ typedef struct s_vars
 	void	*win;
 	void	*img;
 	char	*addr;
-	int	img_width;
-	int	img_height;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
+	int		img_width;
+	int		img_height;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
 	double	max;
 	double	min;
 	double	x;
@@ -25,21 +37,21 @@ typedef struct s_vars
 	double	jul_y;
 	double	a;
 	double	b;
-	int	set;
-	int	av1;
-	int	av2;
+	int		set;
+	int		av1;
+	int		av2;
 }	t_vars;
 
 double	to_real(double coor, t_vars *vars);
-int	mouse_hook(int mousecode, int x, int y, t_vars *vars);
-int	key_hook(int keycode, t_vars *vars);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_atoi(const char *str);
-void	error_handler();
+int		mouse_hook(int mousecode, int x, int y, t_vars *vars);
+int		key_hook(int keycode, t_vars *vars);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+void	error_handler(void);
 void	my_mlx_pixel_put(t_vars *vars, int x, int y, int color);
-int	my_color(int count);
-int	check_conver(double x, double y);
-int	exit_hook();
+int		my_color(int count);
+int		check_conver(double x, double y);
+int		exit_hook(void);
 void	mandelbrot(t_vars *img);
 void	julia(t_vars *img, int val1, int va12);
 
